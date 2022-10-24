49b56f468b26d9b063e4033896f679a33dff3a27
mercury233 <me@mercury233.me>
2022-10-24 21:53:38 +0800

    update

--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php header('Content-Type: text/plain; charset=UTF-8'); ?>
-2022-09-29:.:https://ygopro2.link/downloads.html
-更新WPP3等新卡，修复人机模式，卡包列表
+2022-10-24:.:https://ygopro2.link/downloads.html
+更新PHHY(1111)等新卡
