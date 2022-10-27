771deb4a9c940329347035ed764923c1d207a04b
mercury233 <me@mercury233.me>
2022-10-27 21:56:28 +0800

    new website

--- index.php
+++
@@ -1,1 +0,0 @@
-<?php header('Location: https://ygopro2.lofter.com/', true, 302); ?>
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php header('Content-Type: text/plain; charset=UTF-8'); ?>
-2022-10-24:.:https://ygopro2.link/downloads.html
+2022-10-24:.:https://ygopro2.com/download/
 更新PHHY(1111)等新卡
